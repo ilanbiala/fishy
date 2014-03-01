4cdfc116070e7e92e3e74c08e5ec267b661d5841
Ed <Edward@Edwards-MacBook-Air.local>
2014-03-01 12:48:34 -0500

    added basic enemyFish movement, made fish.move more generic, modified initial fish coordinates
    Added object enemyFish, modified fish.move to make it look at
    conditions specific to each fish (moveLeft, right, up, and down) that
    are now determined each tick in checkMove. To fix: if enemyFish is at
    the same x and/or y coordinate as fish, due to the checks used for
    enemyFish movement and general fish physics, enemyFish “jitters” rather
    than staying still. Also modified the fish starting coordinates so that
    it starts at a random y value, and either the left or right side of the
    screen. To fix: if starting at the right, the fish starts off
    immediately off-screen; the position needs to be offset by the fish
    image’s width.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -8,7 +8,8 @@ var jqCanvas,
 		40: false,
 		lastKey: null
 	},
-	fish;
+	fish,
+    enemyFish;
 var fishSymbolArray = ['/images/fish1.png', '/images/fish2.png', '/images/fish3.png', '/images/fish4.png'];
 
 $(document).ready(function () {
@@ -18,6 +19,7 @@ $(document).ready(function () {
 	canvas.height = window.innerHeight;
 	ctx = canvas.getContext('2d');
 	fish = new Fish();
+    enemyFish = new Fish();
 	$(document).on('keydown', function (e) {
 		if (e.which === 37 || e.which === 38 || e.which === 39 || e.which === 40) {
 			//fish.move(e.which);
@@ -47,10 +49,14 @@ function Fish() {
 	this.symbol.src = fishSymbolArray[Math.floor(Math.random() * 4)]; // which icon to use
 	this.drawn = false;
 	this.position = {
-		x: Math.floor(Math.random() * 2), // 0 = left, 1 = right
-		y: Math.floor(Math.random() * 101) // 0 -> 100, high == higher up the screen
+		x: Math.floor(Math.random() * 2) * canvas.width, // 0 = left, 1 = right
+		y: Math.floor(Math.random() * canvas.height) // 0 -> 100, high == higher up the screen
 	};
 	this.orientation = !this.position.x; // start fish going to center of screen
+    this.moveLeft = false;
+    this.moveRight = false;
+    this.moveUp = false;
+    this.moveDown = false;
 }
 
 Fish.prototype.toString = function () {
@@ -72,21 +78,21 @@ Fish.prototype.move = function () {
 		console.log('speed: ' + this.maxSpeed + ', acceleration: ' + this.acceleration + ', deceleration: ' + this.deceleration);
 	}
 
-	if (keysPressed[37]) { // left key
+	if (this.moveLeft) { // left key
 		this.xSpeed = Math.max(-this.maxSpeed - this.deceleration, this.xSpeed - this.acceleration - this.deceleration); // Increase speed unless at max
 		this.orientation = 0;
 	}
-	if (keysPressed[38]) { // up key
+	if (this.moveUp) { // up key
 		this.ySpeed = Math.max(-this.maxSpeed - this.deceleration, this.ySpeed - this.acceleration - this.deceleration); // Increase speed unless at max
 	}
-	if (keysPressed[39]) { // right key
+	if (this.moveRight) { // right key
 		this.xSpeed = Math.min(this.maxSpeed + this.deceleration, this.xSpeed + this.acceleration + this.deceleration); // Increase speed unless at max
 		this.orientation = 1;
 		// if (this.orientation == 1) {
 		// this.symbol.style.webkitTransform = 'rotateY(180deg)';
 		// }
 	}
-	if (keysPressed[40]) { // down key
+	if (this.moveDown) { // down key
 		this.ySpeed = Math.min(this.maxSpeed + this.deceleration, this.ySpeed + this.acceleration + this.deceleration); // Increase speed unless at max
 	}
 	//Decelerate horizontal
@@ -109,10 +115,36 @@ Fish.prototype.move = function () {
 function clearCanvas(context, canvas) {
 	context.clearRect(0, 0, canvas.width, canvas.height);
 }
+function checkMove(){
+    fish.moveLeft = keysPressed[37];
+    fish.moveUp = keysPressed[38];
+    fish.moveRight = keysPressed[39];
+    fish.moveDown = keysPressed[40];
+    enemyFish.moveLeft = enemyFish.moveUp = enemyFish.moveRight = enemyFish.moveDown = false;
+    if (fish.position.x > enemyFish.position.x)
+    {
+        enemyFish.moveRight = true;
+    }
+    else if (fish.position.x < enemyFish.position.x)
+    {
+        enemyFish.moveLeft = true;
+    }
+    if (fish.position.y > enemyFish.position.y)
+    {
+        enemyFish.moveDown = true;
+    }
+    else if (fish.position.y < enemyFish.position.x)
+    {
+        enemyFish.moveUp = true;
+    }
+}
 
 function renderCanvas() {
+    checkMove()
 	clearCanvas(ctx, canvas);
 	fish.move();
+    enemyFish.move();
 	ctx.drawImage(fish.symbol, fish.position.x, fish.position.y);
+    ctx.drawImage(enemyFish.symbol, enemyFish.position.x, enemyFish.position.y);
 	requestAnimationFrame(renderCanvas);
 }
